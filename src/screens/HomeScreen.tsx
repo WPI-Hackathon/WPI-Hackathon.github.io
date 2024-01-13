39ad4e3ce81c757da78bf7b9f02f3987e16a22c4
megansin <megan_sin@student.wayland.k12.ma.us>
2024-01-13 01:15:52 -0500

    added words to home page

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,8 @@
 export default function HomeScreen() {
   return (
-    <div>hello</div>
+    <div>
+      <h1>Welcome to Rendezview</h1>
+      <h3>A better way to schedule</h3>
+    </div>
   )
 }
