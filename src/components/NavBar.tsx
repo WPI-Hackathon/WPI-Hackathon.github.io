3856fd914ff6f156fe0cb0c1a3baee47e2999285
megansin <megan_sin@student.wayland.k12.ma.us>
2024-01-13 18:14:16 -0500

    wip of dynamic navbar based on login

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getAuth } from 'firebase/auth';
 
 const NavBar: React.FC = () => {
+  const user = getAuth().currentUser;
+
   return (
     <nav className="bg-indigo-600 p-4 absolute top-0 w-full">
       <div className="flex space-x-8">
-        <div>
-          <Link to="/home" className="text-white">Home</Link>
-        </div>
-        <div>
-          <Link to="/calendar" className="text-white">My Calendar</Link>
-        </div>
-        <div>
-          <Link to="/dashboard" className="text-white">Dashboard</Link>
-        </div>
+        <Link to="/home" className={`text-white ${user ? '' : 'hidden'}`}>Home</Link>
+        <Link to="/calendar" className={`text-white ${user ? '' : 'hidden'}`}>My Calendar</Link>
+        <Link to="/dashboard" className={`text-white ${user ? '' : 'hidden'}`}>Dashboard</Link>
+        <Link to="/login" className={`text-white ${user ? 'hidden' : ''}`}>Login</Link>
+        <Link to="/signup" className={`text-white ${user ? 'hidden' : ''}`}>Sign Up</Link>
       </div>
     </nav>
   );
