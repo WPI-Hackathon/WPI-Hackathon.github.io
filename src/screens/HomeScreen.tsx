a9424b0abf93e699ccd7afc9ebcb8d40a666dc83
megansin <megan_sin@student.wayland.k12.ma.us>
2024-01-13 13:24:32 -0500

    fixed format of navbar and home page

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,8 @@
 export default function HomeScreen() {
   return (
-    <div>
-      <h1>Welcome to Rendezview</h1>
-      <h3>A better way to schedule</h3>
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="mb-4">Welcome to Rendezview</h1>
+      <h3 className="">A better way to schedule</h3>
     </div>
-  )
+  );
 }
