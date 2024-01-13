d71da9768ffc0dbda743a3e31e3eb33e4f53fc90
bhnord <b.nordemann@aol.com>
2024-01-12 21:24:39 -0500

    firebase setup

---
+++ src/config/firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKqKS3caRrXryzJr4mJLJGBijtlHI2KpY",
+  authDomain: "when3meet-baa3c.firebaseapp.com",
+  projectId: "when3meet-baa3c",
+  storageBucket: "when3meet-baa3c.appspot.com",
+  messagingSenderId: "541841002814",
+  appId: "1:541841002814:web:5ec4bb3785b9e1e7291c6f"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
