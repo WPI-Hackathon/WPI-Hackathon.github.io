992f1601e56815a2a068ef3372be424a61b97db1
megansin <megan_sin@student.wayland.k12.ma.us>
2024-01-13 20:20:04 -0500

    updated design of create group page

--- src/screens/CreateGroupScreen.tsx
+++ src/screens/CreateGroupScreen.tsx
@@ -53,15 +53,15 @@ export default function CreateGroupScreen() {
   return (
     <div className="pt-20">
       <div className="m-auto">
-        <h1>How to Create a Group</h1>
-        <ul className="list-inside">
-          <li>Enter your desired Group Name</li>
-          <li>Click on the Create Group button</li>
-          <li>Copy the unique key and share this with your group members</li>
-          <li>Your group members need to join your group with this key</li>
+        <h1 className="font-semibold py-6">How to Create a Group</h1>
+        <ul className="list-inside list-decimal">
+          <li className="py-1">Enter your desired Group Name</li>
+          <li className="py-1">Click on the Create Group button</li>
+          <li className="py-1">Copy the unique key and share this with your group members</li>
+          <li className="py-1">Your group members need to join your group with this key</li>
         </ul>
       </div>
-      <div className="mt-12">
+      {/* <div className="mt-12">
         <input type="text" placeholder="Group Name" onChange={event => setGroupName(event.target.value)} />
       </div>
       <button className="my-12" onClick={createGroup}>Click to create Group</button>
@@ -73,7 +73,36 @@ export default function CreateGroupScreen() {
 
           : <p className="m-auto p-2  my-6 "></p>}
         <button onClick={copyKey}>copy key</button>
+      </div> */}
+      <div className="mt-10">
+        <input
+          type="text"
+          placeholder="Group Name"
+          onChange={(event) => setGroupName(event.target.value)}
+          className="px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+        />
       </div>
+      <button
+        className="my-4 px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
+        onClick={createGroup}
+      >
+        Click to create Group
+      </button>
+      <div className="my-10">
+        <p>Unique Key:</p>
+        {key !== "" ? (
+          <p className="w-max m-auto p-2 border-2 border-solid">{key}</p>
+        ) : (
+          <p className="m-auto p-2 my-6"></p>
+        )}
+        <button
+          onClick={copyKey}
+          className="my-4 px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
+        >
+          Copy Key
+        </button>
+      </div>
+
 
     </div>
   )
