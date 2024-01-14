bc882d6fdf025d6e4ac9af8195f4c624ae58e3af
megansin <megan_sin@student.wayland.k12.ma.us>
2024-01-13 19:38:45 -0500

    moved login button to the right and centered navbar links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -28,35 +28,37 @@ const NavBar: React.FC = () => {
 
   return (
     <nav className="bg-indigo-600 p-4 absolute top-0 w-full">
-      <div className="flex space-x-8">
-        <div>
-          <Link to="/home" className="text-white">Home</Link>
-        </div>
-        <div>
-          <Link to="/calendar" className="text-white">My Calendar</Link>
-        </div>
-        <div>
-          <Link to="/dashboard" className="text-white">Dashboard</Link>
-        </div>
-        <div>
-          <Link to="/creategroup" className="text-white">Create Group</Link>
-        </div>
-        <div>
-          <Link to="/joingroup" className="text-white">Join Group</Link>
+      <div className="flex justify-between items-center">
+        <div className="space-x-8">
+          <Link to="/home" className="text-white">
+            Home
+          </Link>
+          <Link to="/calendar" className="text-white">
+            My Calendar
+          </Link>
+          <Link to="/dashboard" className="text-white">
+            Dashboard
+          </Link>
+          <Link to="/creategroup" className="text-white">
+            Create Group
+          </Link>
+          <Link to="/joingroup" className="text-white">
+            Join Group
+          </Link>
         </div>
 
-        {user ?
-          <div>
+        {user ? (
+          <div className="flex items-center">
             <div>
               <button onClick={logout}>Logout</button>
             </div>
-            <div>Hello {user?.displayName}</div>
+            <div className="text-white">Hello {user?.displayName}</div>
           </div>
-          :
-          <button>
-            <Link to="/login" className="text-white">Login</Link>
+        ) : (
+          <button className="bg-white hover:bg-gray-100 text-indigo-600 text-sm">
+            <Link to="/login">Login</Link>
           </button>
-        }
+        )}
       </div>
     </nav>
   );
