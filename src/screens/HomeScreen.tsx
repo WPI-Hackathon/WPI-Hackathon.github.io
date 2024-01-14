bd0980689e3445d06f4cdf920283e550e309ed11
megansin <megan_sin@student.wayland.k12.ma.us>
2024-01-13 20:03:56 -0500

    made name semibold

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,7 +2,7 @@ export default function HomeScreen() {
   return (
     <div className="flex flex-col items-center justify-center pt-48">
       <img src="src/assets/GoatHacksLogo.png" className="h-auto max-w-xs" alt="Logo" />
-      <h1 className="mb-4">Welcome to Rendezview</h1>
+      <h1 className="mb-4 font-semibold">Welcome to Rendezview</h1>
       <h3 className="">A better way to schedule</h3>
     </div>
   );
